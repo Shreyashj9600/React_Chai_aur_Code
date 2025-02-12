f3ae18a6ea936028e65b36746c5e34dcf5bc7312
Shreyashj9600 <139689092+Shreyashj9600@users.noreply.github.com>
2025-02-12 11:51:49 +0000

    how react works

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -18,3 +18,24 @@ const reactElement = {
 const maincontainer = document.getElementById('root')
 
 customRender(reactElement, maincontainer)
+
+
+// test 
+
+// function customRender(reactElement, container) {
+//     let domElement = document.createElement(reactElement.type)
+//     domElement.innerHTML = reactElement.props.Text
+//     container.appendChild(domElement)
+
+// }
+
+// const reactElement = {
+//     type: 'h1',
+//     props: {
+//         "Text": 'Test custom React app'
+//     }
+// }
+
+// const maincontainer = document.getElementById('root')
+
+// customRender(reactElement, maincontainer)
